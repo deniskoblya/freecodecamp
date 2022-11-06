c82c146de8dac7233a9b6f2be503eca3783e5d30
Denys Koblia <denis.koblya@gmail.com>
2022-11-06 10:56:48 +0200

    Create banjo.js

---
+++ banjo.js
@@ -0,0 +1,9 @@
+function play(name) {
+  if (name[0] == "r" || name[0] == "R") {
+    console.log(name + " plays banjo");
+  } else {
+    console.log(name + " does not play banjo");
+  }
+}
+
+play("poland");
